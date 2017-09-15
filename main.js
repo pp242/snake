d81744f335d37b6417ea7021586837931ab3b1bb
Priyam Patel <priyampatel1994@gmail.com>
2017-09-15 13:09:23 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,112 @@
+$(function(){
+
+
+	var $head = $("#head");
+	var direction = 0;
+	var movement = setInterval(function() {});
+	function playGame () {
+    	$(document).keydown(function(event){
+    		clearInterval(movement);
+	    	if(event.keyCode==38)//up
+		    {
+		      	$head.animate({top: "-=20px"},"fast");
+		      	direction = event.keyCode;
+		     	move(direction);
+		      
+		    }
+		    else if(event.keyCode==40)//down
+		    {
+		      	$head.animate({top: "+=20px"},"fast");
+		      	direction = event.keyCode;
+		      	move(direction);
+		      		      
+		    }
+		    else if(event.keyCode==37)//left
+		    {
+		      	$head.animate({left: "-=20px"},"fast");
+		      	direction = event.keyCode;
+		      	move(direction);
+		      
+		    }
+		    else if(event.keyCode==39)//right
+		    {
+		      	$head.animate({left: "+=20px"},"fast");
+		      	direction = event.keyCode;
+		      	move(direction);
+		      
+		    }
+
+	  	});
+	}  	
+
+
+		    $("#buttonLeft").click(function(event){
+		    	$head.animate({
+		      	"left": "-=20px"
+		    	},"fast");
+		 	});
+		    $("#buttonRight").click(function(event){
+		    	$head.animate({
+		      	"left": "+=20px"
+		    	},"fast");
+		    });
+		  	$("#buttonTop").click(function(event){
+		    	$head.animate({
+		      	top: "-=20px"
+		    	},"fast")
+		  	});
+		  	$("#buttonBottom").click(function(event){
+		    	$head.animate({
+		    	  top: "+=20px"
+		    	},"fast")
+		  	});
+
+
+	function move(direction) {
+
+		movement = setInterval(function(){ 
+
+			
+
+	    	if(direction==38)//up
+		    {
+		      	$head.animate({top: "-=20px"},"fast");
+		    }
+		    else if(direction==40)//down
+		    {
+		      	$head.animate({top: "+=20px"},"fast");
+		    }
+		    else if(direction==37)//left
+		    {
+		      	$head.animate({left: "-=20px"},"fast");
+		    }
+		    else if(direction==39)//right
+		    {
+		      	$head.animate({left: "+=20px"},"fast");
+		    }
+
+		}, 500);
+
+	}
+
+
+	function die() {
+		if($head.top: '-20px' )
+			{ 
+			}
+
+	}
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
